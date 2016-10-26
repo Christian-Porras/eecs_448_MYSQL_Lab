db8fa40f6a16cfcf6bf7e4d2a9752d1c91e5d21b
Chris Porras <cjincolorado@comcast.net>
2016-10-25 19:46:42 -0500

    Exercise 6 completed

---
+++ php/ViewUserPosts.php
@@ -0,0 +1,31 @@
+<?php
+  $username = $_POST["Users"];
+
+  $mysqli = new mysqli("mysql.eecs.ku.edu", "cporras", "Password123!", "cporras");
+
+  if ($mysqli->connect_error) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+  }  
+
+  $query = "SELECT post_id,content FROM Posts WHERE author_id ='".$username."'";
+
+  $result = $mysqli->query($query);
+
+  if($result->num_rows > 0 ){
+    echo "<h1>Showing Posts by User: ".$username."</h1>";
+    echo "<table><tr><th>Post ID</th><th>Post content</th></tr>";
+    while($row = $result->fetch_assoc()){
+      echo "<tr><td>".$row["post_id"]."</td><td>".$row["content"]."</td><tr>";
+    }
+    echo "</table>";
+  }
+  else{
+    echo "User ".$username." has no posts.";
+  }
+
+  echo "<button onclick = \"window.location.href='../AdminHome.html'\">Return to Admin Page</button>";
+  echo "<button onclick = \"window.location.href='../ViewUserPosts.html'\">Return to View User Posts Page</button>";
+
+  $mysqli->close();
+?>
